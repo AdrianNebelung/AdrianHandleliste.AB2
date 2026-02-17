7bf722c25c163a85f217727869ae075e83315107
AdrianNebelung <acnebelu@hiof.no>
2026-02-17 09:43:40 +0100

    Fikset problem med htmlFor

--- handleliste/src/components/ShoppingItem.jsx
+++ handleliste/src/components/ShoppingItem.jsx
@@ -3,14 +3,14 @@ function ShoppingItem({ vare, onVareKjopt, onOppdaterAntall }) {
         // På denne li har jeg sett på eksempel ifra AI (Gemini) på hvordan man lager Ternary Operator inni en className. Som gjør at den vil endre styling etter at man har brukt checkbox.
         <li className={`shopping-item ${vare.kjopt ? "shopping-item-kjopt" : ""}`}>
             {/* Her har jeg lagt til to label en input hver. Den første label er for checkbox og den andre er for antallet man kan legge til på varen */}
-            <label className="shopping-item-label" id="checkbox">
-                <input htmlFor="checkbox" type="checkbox" className="shopping-item-checkbox" checked={vare.kjopt} onChange={() => onVareKjopt(vare.id)}/>
+            <label className="shopping-item-label" htmlFor={`checkbox-${vare.id}`}>
+                <input id={`checkbox-${vare.id}`} type="checkbox" className="shopping-item-checkbox" checked={vare.kjopt} onChange={() => onVareKjopt(vare.id)}/>
                 {vare.navn}
             </label>
 
-            <label className="shopping-item-antall" id="antall">
+            <label className="shopping-item-antall" htmlFor={`antall-${vare.id}`}>
                 Antall:
-                <input htmlFor="antall" type="number" className="shopping-item-number" min="1" value={vare.antall} onChange={(e) => onOppdaterAntall(vare.id, e.target.value)}/>
+                <input id={`antall-${vare.id}`} type="number" className="shopping-item-number" min="1" value={vare.antall} onChange={(e) => onOppdaterAntall(vare.id, e.target.value)}/>
             </label>
         </li>
     );
